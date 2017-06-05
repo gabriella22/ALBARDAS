6c3e6b9a50fcef9161e08463765c48a724dd4de6
gabriella22 <jdltjuan@gmail.com>
2017-06-05 16:45:45 -0500

    Add files via upload

---
+++ is.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="whidth=device-width,user-scalable=no,initial-scale:1.0, minimum-scale=1.0"/>
+	<title>Login Control de Combustibles</title>
+	<link href="/usuarios/style/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="/usuarios/style/font-awesome.min.css">
+	<link rel="stylesheet" href="/usuarios/style/font-awesome.css">
+	<link rel="stylesheet" href="/usuarios/style/animate.css">
+	<link rel="stylesheet" type="text/css" href="/usuarios/style/global.css" />
+	<link rel="stylesheet" type="text/css" href="/usuarios/style/global.css" />
+	<meta name="viewport" content="whidth=device-width,initial-scale:1.0, user-scalable=0"/>
+</head>
+<body>
+	<div id="header">
+		<div class="textcenter"><span>Control de Combustibles</span></div>
+	</div>
+	<div class="main">
+		<form class="form" action="" id="formlg">
+	 		<div class="divinitimg">
+	 			<img class="imginit" src="/usuarios/image/logo.svg">
+	 		</div>
+	 		<div class="divusuario">
+	 			<input class=" intext " type="text" name="usuariolg" id='usuaruiolg' placeholder="Usuario" required />
+	 			<input class="intext" type="password" name="passlg" id='passlg' placeholder="contraseña" required />
+	 			<input class="btn btn-primary hover" type="submit" class="botonlg" value="Iniciar Sesion"/>
+	 		</div>
+	 	</form>
+	 </div>
+
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+<script src="scripts/jquery-1.11.1.min.js"></script>
+<script src="scripts/general.js"></script>
+</body>
+</html>
